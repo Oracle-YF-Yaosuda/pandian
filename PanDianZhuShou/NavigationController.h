15933ed7b3c91b31cb23734dd9dfe5f0f20e6511
sk19920518 <350458783@qq.com>
2016-01-19 17:00:49 +0800

    navagation

---
+++ PanDianZhuShou/NavigationController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationController.h
+//  PanDianZhuShou
+//
+//  Created by suokun on 16/1/19.
+//  Copyright © 2016年 sk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationController : UINavigationController
+
+@end
