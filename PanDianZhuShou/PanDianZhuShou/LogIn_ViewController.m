51492e67173f4a7ba731ee8ff38a4309cdfa92e2
xiaolang <langyaoyu@hotmail.com>
2016-01-20 11:05:34 +0800

    登陆接口 不通

--- PanDianZhuShou/PanDianZhuShou/LogIn_ViewController.m
+++ PanDianZhuShou/PanDianZhuShou/LogIn_ViewController.m
@@ -7,7 +7,13 @@
 //
 
 #import "LogIn_ViewController.h"
+
 #import "NavigationController.h"
+
+#import "AFHTTPRequestOperationManager.h"
+#import "WarningBox.h"
+#import "SBJsonWriter.h"
+
 
 @interface LogIn_ViewController ()
 
@@ -17,12 +23,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    _Password_Text.delegate=self;
+    _Username_Text.delegate=self;
+    _Username_Text.text=@"fdfapp";
+    _Password_Text.text=@"123456";
     //设置  textfield  placeholder  颜色
     [self text_placeholder];
     
 }
-
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    if (textField==_Username_Text) {
+        [_Username_Text resignFirstResponder];
+        [_Password_Text becomeFirstResponder];
+    }else{
+        [self.view endEditing:YES];
+        [self LogIn_Button:nil];
+    }
+    return YES;
+}
 //设置  textfield  placeholder  颜色
 -(void)text_placeholder
 {
@@ -36,16 +54,26 @@
 
 - (IBAction)LogIn_Button:(id)sender {
     
+    AFHTTPRequestOperationManager*manager=[AFHTTPRequestOperationManager manager];
+    manager.responseSerializer.acceptableContentTypes=[NSSet setWithObjects:@"application/json",@"text/json",@"text/plate",@"text/html", nil];
+    SBJsonWriter*writer=[[SBJsonWriter alloc] init];
+    NSDictionary*datadic=[NSDictionary dictionaryWithObjectsAndKeys:_Username_Text.text,@"username",_Password_Text.text,@"password", nil];
+    NSString*jsonstring=[writer stringWithObject:datadic];
    
-    NavigationController*chaxun=[[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"navigationcontroller"];
-    
-    [self presentViewController:chaxun animated:YES completion:^{
-        
-//    [self setModalTransitionStyle: UIModalTransitionStyleCrossDissolve];
-        
-    }];
-    
-    
-    
+    NSString*url=@"http://222.171.242.146/stock/stockIntf/login";
+    NSDictionary*dic=[NSDictionary dictionaryWithObjectsAndKeys:jsonstring,@"params", nil];
+ 
+   [manager POST:url parameters:dic success:^(AFHTTPRequestOperation *operation, id responseObject) {
+          NSLog(@"%@",responseObject);
+       if ([[responseObject objectForKey:@"flag"] intValue]==1) {
+           NavigationController*chaxun=[[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"navigationcontroller"];
+           
+           [self presentViewController:chaxun animated:YES completion:^{
+               
+           }];
+       }
+   } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+       NSLog(@"%@",error);
+   }];
 }
 @end
