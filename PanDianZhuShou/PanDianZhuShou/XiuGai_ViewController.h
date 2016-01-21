afba36df16f4219e21bfc3697935a63a3c16a889
sk19920518 <350458783@qq.com>
2016-01-21 08:53:54 +0800

    Revert "Revert "Revert "000"""
    This reverts commit e2419612f8e7906e0ac2abd18cf42a6f60a83a8e.

--- PanDianZhuShou/PanDianZhuShou/XiuGai_ViewController.h
+++ PanDianZhuShou/PanDianZhuShou/XiuGai_ViewController.h
@@ -9,15 +9,5 @@
 #import <UIKit/UIKit.h>
 
 @interface XiuGai_ViewController : UIViewController
-//旧密码
-@property (weak, nonatomic) IBOutlet UITextField *OldPass_Field;
-//新密码
-@property (weak, nonatomic) IBOutlet UITextField *NewPass_Field;
-//确认新密码
-@property (weak, nonatomic) IBOutlet UITextField *NewPass1_Field;
-//确认
-@property (weak, nonatomic) IBOutlet UIButton *QueRen_Button;
-- (IBAction)QueRen_Button:(id)sender;
-
 
 @end
