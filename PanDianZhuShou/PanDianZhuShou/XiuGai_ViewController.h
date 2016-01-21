be4dad96496daff4f1714531a1589a7c78c9558f
sk19920518 <350458783@qq.com>
2016-01-21 10:20:32 +0800

    更改密码

--- PanDianZhuShou/PanDianZhuShou/XiuGai_ViewController.h
+++ PanDianZhuShou/PanDianZhuShou/XiuGai_ViewController.h
@@ -9,5 +9,15 @@
 #import <UIKit/UIKit.h>
 
 @interface XiuGai_ViewController : UIViewController
+//旧密码
+@property (weak, nonatomic) IBOutlet UITextField *Oldpass_Field;
+//新密码
+@property (weak, nonatomic) IBOutlet UITextField *Newpass_Field;
+//确认新密码
+@property (weak, nonatomic) IBOutlet UITextField *Newpass_Field_2;
+//确认
+@property (weak, nonatomic) IBOutlet UIButton *QueRen_Button;
+- (IBAction)QueRen_Button:(id)sender;
+
 
 @end
