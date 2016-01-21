8eae2b22cc5fded948367b079d8ca8609b9dbd01
xiaolang <langyaoyu@hotmail.com>
2016-01-21 09:18:28 +0800

    重新来的接口

--- PanDianZhuShou/PanDianZhuShou/LogIn_ViewController.h
+++ PanDianZhuShou/PanDianZhuShou/LogIn_ViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface LogIn_ViewController : UIViewController
+@interface LogIn_ViewController : UIViewController<UITextFieldDelegate>
 
 @property (weak, nonatomic) IBOutlet UITextField *Username_Text;
 
--- PanDianZhuShou/PanDianZhuShou/LogIn_ViewController.m
+++ PanDianZhuShou/PanDianZhuShou/LogIn_ViewController.m
@@ -7,7 +7,12 @@
 //
 
 #import "LogIn_ViewController.h"
+
 #import "NavigationController.h"
+
+#import "AFHTTPRequestOperationManager.h"
+#import "WarningBox.h"
+#import "SBJsonWriter.h"
 
 @interface LogIn_ViewController ()
 
@@ -17,12 +22,24 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    _Password_Text.delegate=self;
+    _Username_Text.delegate=self;
+    _Username_Text.text=@"fdfapp";
+    _Password_Text.text=@"123456";
     //设置  textfield  placeholder  颜色
     [self text_placeholder];
     
 }
-
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    if (textField==_Username_Text) {
+        [_Username_Text resignFirstResponder];
+        [_Password_Text becomeFirstResponder];
+    }else{
+        [self.view endEditing:YES];
+        [self LogIn_Button:nil];
+    }
+    return YES;
+}
 //设置  textfield  placeholder  颜色
 -(void)text_placeholder
 {
@@ -36,15 +53,44 @@
 
 - (IBAction)LogIn_Button:(id)sender {
     
-   
-    NavigationController*chaxun=[[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"navigationcontroller"];
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.responseSerializer.acceptableContentTypes=[NSSet setWithObjects:@"application/json",@"text/json",@"text/plate",@"text/html", nil];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
     
-    [self presentViewController:chaxun animated:YES completion:^{
+    
+    manager.requestSerializer = [AFJSONRequestSerializer serializer];
+    
+    NSString *url = @"http://222.171.242.146/stock/stockIntf/login";
+    
+    NSDictionary *params = @{@"username":@"fdfapp",@"password":@"123456"};
+    
+    [manager POST:url parameters:params success:^(AFHTTPRequestOperation *operation, id responseObject) {
         
-//    [self setModalTransitionStyle: UIModalTransitionStyleCrossDissolve];
+        
+        NSData *haha = responseObject;
+        NSString *hehe =  [[NSString alloc]initWithData:haha encoding:NSUTF8StringEncoding];
+        NSLog(@"------------%@",hehe);
+        
+        
+//        if ([[responseObject objectForKey:@"flag"] intValue]==1) {
+//            NavigationController*chaxun=[[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"navigationcontroller"];
+//            
+//            [self presentViewController:chaxun animated:YES completion:^{
+//                
+//                //    [self setModalTransitionStyle: UIModalTransitionStyleCrossDissolve];
+//                
+//            }];
+        
+
+//        }
+        
+        
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        
+        NSLog(@"%@",error);
         
     }];
-    
     
     
 }
