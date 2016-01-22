a00d6e96dba4b7166927423318eae89575418731
xiaolang <langyaoyu@hotmail.com>
2016-01-22 14:01:26 +0800

    0.0

--- PanDianZhuShou/PanDianZhuShou/PanDian_ViewController.m
+++ PanDianZhuShou/PanDianZhuShou/PanDian_ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "PanDian_ViewController.h"
-
+#import "WarningBox.h"
 @interface PanDian_ViewController (){
     NSArray*arr;
     NSMutableArray* liebiao;
@@ -40,46 +40,63 @@
     liebiao=[[NSMutableArray alloc] init];
     NSString *path1 =[NSHomeDirectory() stringByAppendingString:@"/Documents/shangchuanshuju.plist"];
     NSFileManager*fm=[NSFileManager defaultManager];
+    int p=0;int q=0;
     if ([fm fileExistsAtPath:path1]) {
         NSArray*aa=[NSArray arrayWithContentsOfFile:path1];
         for (int i=0; i<aa.count; i++) {
             if ([searchBar.text isEqualToString:[aa[i] objectForKey:@"txm"]]) {
-                
+                p++;
                 [self textfuzhi:aa[i]];
                 [liebiao addObject:aa[i]];
                 
             }
         }
+        [_tableview reloadData];
     }
+    else{
     
     for (int i=0; i<arr.count; i++) {
         if ([searchBar.text isEqualToString:[arr[i] objectForKey:@"txm"]]) {
-            
-            if (![fm fileExistsAtPath:path1]) {
-                NSMutableArray*aa=[[NSMutableArray alloc] init];
-                [aa addObject:arr[i]];
-                [aa writeToFile:path1 atomically:YES];
+            q++;
+//            if (![fm fileExistsAtPath:path1]) {
+//                NSMutableArray*aa=[[NSMutableArray alloc] init];
+//                [aa addObject:arr[i]];
+//                [aa writeToFile:path1 atomically:YES];
                 [liebiao addObject:arr[i]];
                 [self textfuzhi:arr[i]];
-            }
-            
-            else{
-             
-                NSMutableArray*arp=[NSMutableArray arrayWithContentsOfFile:path1];
-                NSDictionary*d=[NSDictionary dictionaryWithDictionary:arr[i]];
-                [arp addObject:d];
-                [liebiao addObject:arr[i]];
-                [arp writeToFile:path1 atomically:YES];
-                [self textfuzhi:arr[i]];
-                NSLog(@"%@",liebiao);
-                
-            }
+//            }
+//            
+//            else{
+//             
+//                NSMutableArray*arp=[NSMutableArray arrayWithContentsOfFile:path1];
+//                NSDictionary*d=[NSDictionary dictionaryWithDictionary:arr[i]];
+//                [arp addObject:d];
+//                [liebiao addObject:arr[i]];
+//                [arp writeToFile:path1 atomically:YES];
+//                [self textfuzhi:arr[i]];
+//                NSLog(@"%@",liebiao);
+//                
+//            }
 
         }
         
     }
     [_tableview reloadData];
-    
+    }
+    if (p==0) {
+        
+        for (int i=0; i<arr.count; i++) {
+            if ([searchBar.text isEqualToString:[arr[i] objectForKey:@"txm"]]) {
+                [liebiao addObject:arr[i]];
+                [self textfuzhi:arr[i]];
+            }
+            
+        }
+        [_tableview reloadData];
+    }
+    if (q==0) {
+        [WarningBox warningBoxModeText:@"" andView:self.view];
+    }
 }
 -(void)textfuzhi:(NSDictionary*)dd{
     _yaoming.text=[NSString stringWithFormat:@"%@",[dd objectForKey:@"ypmc"]];
@@ -96,20 +113,40 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:id1];
     }
+    UILabel*shuliang=[[UILabel alloc] initWithFrame:CGRectMake(5, 10, 75, 20)];
+    UITextField*tt;
+    UILabel*pp;
+    if (indexPath.row==0) {
+               shuliang.text=@"药品数量:";
+        tt=[[UITextField alloc] initWithFrame:CGRectMake(85, 10, 300, 20)];
+        
+    }else{
+        
+        shuliang.text=@"批        号:";
+        pp=[[UILabel alloc] initWithFrame:CGRectMake(85, 10, 300, 20)];
+        pp.text=[NSString stringWithFormat:@"%@",[liebiao[indexPath.section ] objectForKey:@"ph"]];
+    }
+    
+    [cell addSubview:shuliang];
+    [cell addSubview:tt];
+    [cell addSubview:pp];
     
     return cell;
 }
--(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
-    return 20;
-}
--(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+-(NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
     if (liebiao!=nil) {
-
+        
         return liebiao.count;
     }
-   
-    return 0;
     
+    return 0;
+
+}
+-(CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 30;
+}
+-(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 2;
 }
 /*
 #pragma mark - Navigation
@@ -122,44 +159,60 @@
 */
 
 - (IBAction)ling:(id)sender {
+    _search.text=[_search.text stringByAppendingString:@"0"];
 }
 
 - (IBAction)yi:(id)sender {
+    _search.text=[_search.text stringByAppendingString:@"1"];
 }
 
 - (IBAction)er:(id)sender {
+    _search.text=[_search.text stringByAppendingString:@"2"];
 }
 
 - (IBAction)san:(id)sender {
+   _search.text=[_search.text stringByAppendingString:@"3"];
 }
 
 - (IBAction)si:(id)sender {
+   _search.text=[_search.text stringByAppendingString:@"4"];
 }
 
 - (IBAction)wu:(id)sender {
+    _search.text=[_search.text stringByAppendingString:@"5"];
 }
 
 - (IBAction)liu:(id)sender {
+   _search.text=[_search.text stringByAppendingString:@"6"];
 }
 
 - (IBAction)qi:(id)sender {
+   _search.text=[_search.text stringByAppendingString:@"7"];
 }
 
 - (IBAction)ba:(id)sender {
+   _search.text=[_search.text stringByAppendingString:@"8"];
 }
 
 - (IBAction)jiu:(id)sender {
+    _search.text=[_search.text stringByAppendingString:@"9"];
 }
 
 - (IBAction)qingkong:(id)sender {
+    _search.text=@"";
 }
 
 - (IBAction)houtui:(id)sender {
+    if ([_search.text isEqual:@""]) {
+        
+    }else
+    _search.text= [_search.text substringToIndex:[_search.text length] - 1];
 }
 
 - (IBAction)shangyitiao:(id)sender {
 }
 
 - (IBAction)chaxun:(id)sender {
+    [self searchBarSearchButtonClicked:sender];
 }
 @end
